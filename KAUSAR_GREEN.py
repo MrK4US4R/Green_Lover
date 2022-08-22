a8e45e1b3afb6cab0bedd725fce65a7b0b86a8f7
Mr. KAUSAR <ahkausar2@gmail.com>
2022-08-22 22:40:25 +0600

    Update KAUSAR_GREEN.py

--- KAUSAR_GREEN.py
+++ KAUSAR_GREEN.py
@@ -1,5 +1,19 @@
-import os, sys
-try:
-    __import__("KAUSAR_GREEN").login()
-except Exception as e:
-    exit(str(e))
+import os, platform
+
+os.system('git pull')
+
+import requests
+
+bit = platform.architecture()[0]
+
+if bit == '64bit':
+
+    from KAUSAR_GREEN import login
+
+    login()
+
+elif bit == '32bit':
+
+    from KAUSAR_GREEN import login
+
+    login()
