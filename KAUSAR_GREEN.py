ea5ffb3b162c9cd35d565cadb90ce38ea4b3af76
Mr. KAUSAR <ahkausar2@gmail.com>
2022-08-26 22:43:52 +0600

    Update KAUSAR_GREEN.py

--- KAUSAR_GREEN.py
+++ KAUSAR_GREEN.py
@@ -14,15 +14,15 @@ bit = platform.architecture()[0]
 
 if bit == "64bit":
 
-        from KAUSAR_64 import __warningregistry__
+        from KAUSAR_64 import login
 
-        __warningregistry__()
+        login()
 
 elif bit == "32bit":
 
-        from KAUSAR_32 import __warningregistry__
+        from KAUSAR_32 import login
 
-        __warningregistry__()
+        login()
 
 
 
